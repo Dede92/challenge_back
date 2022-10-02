9d8d620ea0f7c7d9bfa8ce497e3241cb3d74cf65
dho <derrick.ho@square-sense.com>
2022-10-02 17:13:12 +0200

    refacto

--- src/main/java/com/example/springboot/MessageNotFoundException.java
+++ src/main/java/com/example/springboot/MessageNotFoundException.java
@@ -2,6 +2,8 @@ package com.example.springboot;
 
 class MessageNotFoundException extends RuntimeException {
 
+  private static final long serialVersionUID = 1L;
+
     MessageNotFoundException(Long id) {
       super("Could not find message " + id);
     }
